8476a082a3a32fdf0301bd79d19a71ed5f35ac66
nikandrovaelena93@gmail.com <gmi1987@gmail.com>
2024-10-24 16:43:02 +0400

    fix updated DBstackIcons.js to make more relevant stack positions

--- src/DB/DB-stackIcons/DB-stackIcons.js
+++ src/DB/DB-stackIcons/DB-stackIcons.js
@@ -1,10 +1,10 @@
 const StackIconsDB = [
-    { name: 'html', route: 'html.svg', alt: 'html', href: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-    { name: 'css', route: 'css.svg', alt: 'css', href: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-    { name: 'scss', route: 'scss.svg', alt: 'scss', href: 'https://sass-lang.com/' },
     { name: 'js', route: 'js.svg', alt: 'js', href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
     { name: 'ts', route: 'ts.svg', alt: 'ts', href: 'https://www.typescriptlang.org/' },
     { name: 'react', route: 'reactJs.svg', alt: 'reactJS', href: 'https://react.dev/' },
+    { name: 'html', route: 'html.svg', alt: 'html', href: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+    { name: 'css', route: 'css.svg', alt: 'css', href: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+    { name: 'scss', route: 'scss.svg', alt: 'scss', href: 'https://sass-lang.com/' },
     { name: 'vite', route: 'vite.svg', alt: 'vite', href: 'https://vite.dev/' },
     { name: 'webpack', route: 'webpack.svg', alt: 'webpack', href: 'https://webpack.js.org/' },
     { name: 'gulp', route: 'gulp.svg', alt: 'gulp', href: 'https://gulpjs.com/' },
