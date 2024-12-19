b11f14d10bb827df386c5bb51e5caae41d915684
nikandrovaelena93@gmail.com <gmi1987@gmail.com>
2024-12-19 22:52:51 +0400

    feat added react transition group library to hidden and shown stack elements

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,78 +1,80 @@
 import { useState } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import WebSitePreview from '../../components/WebSitePreview/WebSitePreview';
 import myProjects from '../../DB/DB-myProjects';
 
 import './Projects.scss';
 
 const Projects = () => {
-    // Стейт для отслеживания раскрытых проектов
     const [expandedProjects, setExpandedProjects] = useState({});
 
-    // Функция для переключения состояния (раскрыть/скрыть стек)
     const toggleStack = (projectName) => {
         setExpandedProjects((prev) => ({
-            ...prev, // Копируем существующий стейт
-            [projectName]: !prev[projectName], // Инвертируем состояние для выбранного проекта
+            ...prev,
+            [projectName]: !prev[projectName],
         }));
     };
 
     const SHOWED_STACK_NUMBER = 8;
 
     return (
-        <section className='projects'>
+        <section className="projects">
             <div className="projects__container">
-                <h2 className='visually-hidden'>My commercial projects</h2>
-                <h3>Here are my commercial projects available for public display <b>(not covered by NDA):</b></h3>
-                <ul className='projects__list'>
+                <h2 className="visually-hidden">My commercial projects</h2>
+                <h3>
+                    Here are my commercial projects available for public display <b>(not covered by NDA):</b>
+                </h3>
+                <ul className="projects__list">
                     {myProjects.map((project) => {
                         const isExpanded = expandedProjects[project.projectName] || false;
 
                         return (
-                            <li
-                                className='projects__item'
-                                key={project.projectName}
-                            >
-                                <h4
-                                    className='projects__description'
-                                >
-                                    {project.projectName}
-                                </h4>
-                                <div className='projects__stack-wrapper'>
+                            <li className="projects__item" key={project.projectName}>
+                                <h4 className="projects__description">{project.projectName}</h4>
+                                <div className="projects__stack-wrapper">
                                     <div>
-                                        {/* Отображаем либо первые 7 технологии, либо весь стек */}
-                                        {project.stack
-                                            ?.slice(0, isExpanded ? undefined : SHOWED_STACK_NUMBER)
-                                            .map((technology) => (
-                                                <div className='projects__tech' key={technology}>
-                                                    <p>{technology}</p>
-                                                </div>
-                                            ))}
+                                        {/* Анимация списка технологий */}
+                                        <TransitionGroup>
+                                            {project.stack
+                                                ?.slice(0, isExpanded ? undefined : SHOWED_STACK_NUMBER)
+                                                .map((technology) => (
+                                                    <CSSTransition
+                                                        key={technology}
+                                                        timeout={300}
+                                                        classNames="fade"
+                                                    >
+                                                        <div className="projects__tech">
+                                                            <p>{technology}</p>
+                                                        </div>
+                                                    </CSSTransition>
+                                                ))}
+                                        </TransitionGroup>
 
-                                        {/* Если технологий больше 7, показываем троеточие и кнопку */}
-                                        {project.stack?.length > SHOWED_STACK_NUMBER && !isExpanded && <span className="projects__ellipsis">...</span>}
+                                        {/* Троеточие */}
+                                        {!isExpanded && project.stack?.length > SHOWED_STACK_NUMBER && (
+                                            <span className="projects__ellipsis">...</span>
+                                        )}
 
+                                        {/* Кнопка */}
                                         {project.stack?.length > SHOWED_STACK_NUMBER && (
                                             <button
-                                                className='projects__btn'
+                                                className="projects__btn"
                                                 onClick={() => toggleStack(project.projectName)}
-                                                type='button'
+                                                type="button"
                                             >
                                                 {isExpanded ? 'Show less' : 'Show all stack'}
                                             </button>
                                         )}
                                     </div>
                                     <a
-                                        className='projects__link'
+                                        className="projects__link"
                                         href={project.projectURL}
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
-                                        {/* {project.projectURL} */}
-                                        <div
-                                            className='projects__img-container'
-                                        >
+                                        <div className="projects__img-container">
                                             <WebSitePreview
-                                                className='projects__img'
+                                                className="projects__img"
                                                 url={project.projectURL}
                                                 alt={`screen shot of ${project.projectName}`}
                                             />
@@ -80,12 +82,12 @@ const Projects = () => {
                                     </a>
                                 </div>
                             </li>
-                        )
+                        );
                     })}
                 </ul>
             </div>
-        </section >
-    )
-}
+        </section>
+    );
+};
 
 export default Projects;
