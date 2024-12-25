e4f41483f8aef1b32d7f546b82f8b4117ae686c0
nikandrovaelena93@gmail.com <gmi1987@gmail.com>
2024-12-25 22:12:47 +0400

    feat: MUI library added

--- src/DB/DB-stackIcons.js
+++ src/DB/DB-stackIcons.js
@@ -4,17 +4,18 @@ const StackIconsDB = [
     { name: 'react', route: 'reactJs.svg', alt: 'reactJS', href: 'https://react.dev/' },
     { name: 'redux', route: 'redux.svg', alt: 'redux', href: 'https://redux.js.org/' },
     { name: 'vite', route: 'vite.svg', alt: 'vite', href: 'https://vite.dev/' },
-    { name: 'html', route: 'html.svg', alt: 'html', href: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+    { name: 'webpack', route: 'webpack.svg', alt: 'webpack', href: 'https://webpack.js.org/' },
+    { name: 'gulp', route: 'gulp.svg', alt: 'gulp', href: 'https://gulpjs.com/' },
     { name: 'bem', route: 'bem.svg', alt: 'bem', href: 'https://en.bem.info/' },
+    { name: 'html', route: 'html.svg', alt: 'html', href: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
     { name: 'css', route: 'css.svg', alt: 'css', href: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
     { name: 'scss', route: 'scss.svg', alt: 'scss', href: 'https://sass-lang.com/' },
     { name: 'css-modules', route: 'css-modules.svg', alt: 'css modules', href: 'https://github.com/css-modules/css-modules' },
-    { name: 'webpack', route: 'webpack.svg', alt: 'webpack', href: 'https://webpack.js.org/' },
-    { name: 'gulp', route: 'gulp.svg', alt: 'gulp', href: 'https://gulpjs.com/' },
-    { name: 'pug', route: 'pug.svg', alt: 'pug', href: 'https://pugjs.org/api/getting-started.html' },
     { name: 'git', route: 'git.svg', alt: 'git', href: 'https://git-scm.com/' },
     { name: 'github', route: 'github.svg', alt: 'github', href: 'https://github.com/' },
+    { name: 'gitlab', route: 'gitlab.svg', alt: 'gitlab', href: 'https://gitlab.com/' },
     { name: 'figma', route: 'figma.svg', alt: 'figma', href: 'https://www.figma.com/' },
+    { name: 'pug', route: 'pug.svg', alt: 'pug', href: 'https://pugjs.org/api/getting-started.html' },
 ];
 
 export default StackIconsDB;
