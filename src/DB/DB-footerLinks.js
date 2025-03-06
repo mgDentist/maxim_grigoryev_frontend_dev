260932b89e49dc778910911ea596b83fad4db1a4
nikandrovaelena93@gmail.com <gmi1987@gmail.com>
2025-03-06 13:11:09 +0700

    fix: putting stack icons to specific folder

--- src/DB/DB-footerLinks.js
+++ src/DB/DB-footerLinks.js
@@ -1,8 +1,8 @@
 const footerLinks = [
-    { name: 'GitHub', url: 'https://github.com/mgDentist', iconSvgRoute: 'github.svg', alt: 'github' },
-    { name: 'Linkedin', url: 'https://www.linkedin.com/in/maximgrigorev/', iconSvgRoute: 'linkedin.svg', alt: 'linkedin' },
-    { name: 'Instagram', url: 'https://www.instagram.com/maximize777/profilecard/?igsh=ZDVxZGx2bmNhcmFr', iconSvgRoute: 'instagram.svg', alt: 'instagram' },
-    { name: 'Telegram', url: '#', iconSvgRoute: 'telegram.svg', alt: 'telegram' },
+    { name: 'GitHub', url: 'https://github.com/mgDentist', iconSvgRoute: 'stack-icons/github.svg', alt: 'github' },
+    { name: 'Linkedin', url: 'https://www.linkedin.com/in/maximgrigorev/', iconSvgRoute: 'stack-icons/linkedin.svg', alt: 'linkedin' },
+    { name: 'Instagram', url: 'https://www.instagram.com/maximize777/profilecard/?igsh=ZDVxZGx2bmNhcmFr', iconSvgRoute: 'stack-icons/instagram.svg', alt: 'instagram' },
+    { name: 'Telegram', url: '#', iconSvgRoute: 'stack-icons/telegram.svg', alt: 'telegram' },
 ];
 
 export default footerLinks;
