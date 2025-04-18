8c12226fb0f123744381e6d9ec4688426f71cb98
Maxim Grigorev <gmi1987@gmail.com>
2025-04-18 21:51:29 +0700

    fix: added sitemap.xml via vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,15 @@ export default defineConfig({
         dest: ''
       }
     ]
+  }),
+  sitemap({
+    hostname: 'https://maxgrigorev.dev',
+    routes: [
+      '/',
+      '/my_path',
+      '/projects',
+      '/contacts',
+    ]
   })
   ],
-})
+});
