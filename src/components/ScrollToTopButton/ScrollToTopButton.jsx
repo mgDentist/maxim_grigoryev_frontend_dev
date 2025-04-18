d14573c1cf8ae63d3a20d7b18fe90d068ff8044c
Maxim Grigorev <gmi1987@gmail.com>
2025-04-18 17:48:52 +0700

    feat: added a scroll top button component w/styles

---
+++ src/components/ScrollToTopButton/ScrollToTopButton.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+const ScrollToTopButton = () => {
+    const SCROLL_THRESHOLD = 250;
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > SCROLL_THRESHOLD) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        }
+        window.addEventListener('scroll', toggleVisibility);
+
+        return () => window.removeEventListener('scroll', toggleVisibility);
+
+    }, [])
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    return (
+        <button
+            className={`scroll-to-top-button ${isVisible ? 'visible' : 'hidden'}`}
+            onClick={scrollToTop}
+            aria-label="Scroll to top"
+        >
+            ↑
+        </button>
+    )
+}
+
+export default ScrollToTopButton;
