31d2cac50650c5f5a8906ccbf979f273a54c6a2a
Maxim Grigorev <gmi1987@gmail.com>
2025-05-14 22:20:14 +0700

    fix: changed telegram external link

--- src/DB/DB-contacts-addresses.js
+++ src/DB/DB-contacts-addresses.js
@@ -1,13 +1,13 @@
 const contactsAddresses = [
     { key: 'name', label: 'Name', description: 'Maxim Grigoryev', },
-    { key: 'languages', label: 'My languages', description: 'English 🇬🇧 | Russian 🇷🇺 | Spanish 🇪🇸'},
+    { key: 'languages', label: 'My languages', description: 'English 🇬🇧 | Russian 🇷🇺 | Spanish 🇪🇸' },
     { key: 'location', label: 'Current location', description: 'Phuket, Thailand (TH) 🇹🇭', },
     { key: 'timeZone', label: 'Time zone', description: 'GMT+7', },
     { key: 'email', label: 'E-mail', description: 'maxgrigorev.front@gmail.com', },
 ];
 
 const contactsQRs = [
-    { name: 'Telegram', route: 'qr-codes/tg-qr.jpg', url: 'https://t.me/maksimi_GR', alt: 'telegram address qr' },
+    { name: 'Telegram', route: 'qr-codes/tg-qr.jpg', url: 'https://t.me/maksimgrigorev09', alt: 'telegram address qr' },
     { name: 'GitHub', route: 'qr-codes/gh-qr.jpg', url: 'https://github.com/mgDentist', alt: 'github address qr' },
     { name: 'Linkedin', route: 'qr-codes/in-qr.jpg', url: 'https://www.linkedin.com/in/maximgrigorev/', alt: 'linkedin address qr' },
     { name: 'Instagram', route: 'qr-codes/inst-qr.jpg', url: 'https://www.instagram.com/maximize777/profilecard/?igsh=ZDVxZGx2bmNhcmFr', alt: 'instagram address qr' },
