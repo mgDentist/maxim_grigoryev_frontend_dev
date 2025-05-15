4fd238c1d7278d56c1148394ee6e864cafa27370
Maxim Grigorev <gmi1987@gmail.com>
2025-05-15 21:25:01 +0700

    feat: added some new projects

--- src/DB/DB-myProjects.js
+++ src/DB/DB-myProjects.js
@@ -1,18 +1,28 @@
 const myProjects = [
     {
+        projectName: 'MINECRAFT Web Client',
+        projectURL: 'https://github.com/mgDentist/minecraft-web-client',
+        stack: ['ROLE: React UI & Core Developer Maintainer', 'Open Source', 'ReactJS', 'TypeScript', 'RSbuild', 'Adaptive layout', 'CSS', 'Animation with Framer Motion'],
+    },
+    {
         projectName: 'MG Frontend Developer',
         projectURL: 'https://maxgrigorev.dev/',
         stack: ['ReactJS', 'Redux', 'JS', 'Adaptive layout', 'SCSS', 'Animation with @keyframe', 'React Transition Group', 'Vite', 'Component-based architecture with BEM methodology', 'Traditional React component structure with BEM CSS', 'React components with scoped BEM styling', 'Pixel Perfect'],
     },
     {
-        projectName: 'Albert Safin private page',
+        projectName: 'Albert Safin',
         projectURL: 'https://albertsafin.com/',
         stack: ['JS', 'HTML', 'SCSS', 'Gulp', ' Images sizes optimization', 'All content images retinization', 'Cursors customization', 'Pixel Perfect'],
     },
     {
-        projectName: 'Dive Sea promo page',
+        projectName: 'Dive Sea',
         projectURL: 'https://dive-sea-nuq1.vercel.app/',
         stack: ['React', 'Semantic layout', 'SCSS', 'Webpack', 'Animations - @keyframe', 'Images sizes optimization', 'All content images retinization', 'Ultra wide desktops supported (2K, 4K)', 'Pixel Perfect'],
+    },
+    {
+        projectName: 'Pet Shelter',
+        projectURL: 'https://mst-test-assignment-mg.vercel.app/',
+        stack: ['PURE HTML', 'PURE CSS', 'Semantic layout', 'CSS Animations', 'Images sizes optimization', 'All content images retinization', 'FULL RESPONSIVE', 'Pixel Perfect', 'Figma', 'Mobile First', 'Image generation with AI'],
     },
     {
         projectName: 'Pretty Paw',
