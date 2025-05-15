3eecda129c5136e5ee3797758e014a3915101714
Maxim Grigorev <gmi1987@gmail.com>
2025-05-15 21:26:32 +0700

    feat: the year at footer is auto-updated now

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,11 @@ import LinksFooter from "../LinksFooter/LinksFooter";
 import './Footer.scss';
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="footer">
-            <p>Copyright © 2024 Maksim Grigorev</p>
+            <p>Copyright © {currentYear} Maksim Grigorev</p>
             <p>Coded with ❤️ by MG</p>
             <LinksFooter />
         </footer>
